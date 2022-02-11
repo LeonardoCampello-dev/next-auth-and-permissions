961d872b4ca8734ad05185d676e501596fa00f68
Leonardo Campello <leonardocampello.dev@gmail.com>
2022-02-11 09:32:11 -0300

    feat: retrieving authentication state

--- src/contexts/auth-context.tsx
+++ src/contexts/auth-context.tsx
@@ -1,10 +1,11 @@
 import { useRouter } from 'next/router'
 
-import { createContext, FC, useState } from 'react'
+import { setCookie, parseCookies } from 'nookies'
+import { createContext, FC, useEffect, useState } from 'react'
 
 import { api } from '../services/api'
 
-type User = {
+interface User {
   email: string
   permissions: string[]
   roles: string[]
@@ -21,6 +22,13 @@ type AuthContextStore = {
   isAuthenticated: boolean
 }
 
+interface Session {
+  token: string
+  refreshToken: string
+}
+
+type AuthResponse = Session & User
+
 export const AuthContext = createContext<AuthContextStore>({} as AuthContextStore)
 
 export const AuthProvider: FC = ({ children }) => {
@@ -32,20 +40,44 @@ export const AuthProvider: FC = ({ children }) => {
 
   const signIn = async ({ email, password }: SignInCredentials) => {
     try {
-      const response = await api.post('sessions', {
+      const response = await api.post<AuthResponse>('sessions', {
         email,
         password
       })
 
-      const { permissions, roles } = response.data
+      const { permissions, roles, token, refreshToken } = response.data
 
       setUser({ email, permissions, roles })
+
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
+
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
 
       router.push('/dashboard')
     } catch (error) {
       console.info(error)
     }
   }
+
+  useEffect(() => {
+    const { 'nextauth.token': token } = parseCookies()
+
+    if (token) {
+      api.get<User>('/me').then(response => {
+        const { email, permissions, roles } = response.data
+
+        setUser({ email, permissions, roles })
+      })
+    }
+  }, [])
 
   return (
     <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
